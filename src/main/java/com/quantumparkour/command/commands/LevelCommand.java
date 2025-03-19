e709332340de0adb7cd9263950ad84e28e2e9b8b
olvend <olve@nystad.net>
2025-03-19 22:34:09 +0100

    Add /level list

--- src/main/java/com/quantumparkour/command/commands/LevelCommand.java
+++ src/main/java/com/quantumparkour/command/commands/LevelCommand.java
@@ -8,9 +8,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LevelCommand implements QuantumCommand {
     @Override
@@ -73,11 +74,44 @@ public class LevelCommand implements QuantumCommand {
     }
 
     private void list(CommandSender sender, String[] args) {
-        sender.sendRichMessage("Loaded levels:");
-        QuantumParkour.getLevelManager().getLevels()
-                .stream()
-                .map(level -> " • <click:run_command:'/say hi'>" + level.getName() + "</click>")
+        if (args.length > 2) {
+            sender.sendRichMessage("<red>Too many arguments.");
+            return;
+        }
+        List<Level> levels = QuantumParkour.getLevelManager().getLevels();
+        int pageNumber = 1;
+        int pageAmount = (levels.size() + 9) / 10;
+        if (args.length > 1) {
+            if (args[1].chars().allMatch(Character::isDigit)) {
+                try {
+                    pageNumber = Integer.decode(args[args.length - 1]);
+                } catch (NumberFormatException exception) {
+                    sender.sendRichMessage("<red>'" + args[1] + "' is not a valid page number.");
+                    return;
+                }
+                if (pageNumber <= 0) {
+                    sender.sendRichMessage("<red>'" + args[1] + "' is not a valid page number.");
+                    return;
+                }
+            } else {
+                sender.sendRichMessage("<red>'" + args[1] + "' is not a valid page number.");
+                return;
+            }
+        }
+        if (pageNumber > pageAmount) {
+            sender.sendRichMessage("<red>Page '" + args[1] + "' does not exist.");
+            return;
+        }
+        int start = (pageNumber - 1) * 10;
+        int end = start + 10;
+        sender.sendRichMessage("<#00DFFF>--------- <white>Levels (" + pageNumber + "/" + pageAmount + ")<#00DFFF> ---------");
+        levels.stream()
+                .sorted(Comparator.comparing(Level::getName))
+                .skip(start)
+                .limit(end - start)
+                .map(level -> " • <hover:show_text:'/level settings " + level.getName() + "'><click:run_command:/level settings " + level.getName() + ">" + level.getName() + "</click></hover>")
                 .forEach(sender::sendRichMessage);
+        sender.sendRichMessage(pageNumber == pageAmount ? "<#00DFFF>------------------------------" : "<#00DFFF>--------- <white>/level list " + (pageNumber + 1) + "<#00DFFF> ---------");
     }
 
     private void settings(CommandSender sender, String[] args) {
@@ -96,6 +130,11 @@ public class LevelCommand implements QuantumCommand {
                 return QuantumParkour.getLevelManager().getLevels()
                         .stream()
                         .map(Level::getName)
+                        .collect(Collectors.toList());
+            }
+            if (args[0].equalsIgnoreCase("list")) {
+                return IntStream.rangeClosed(1, (QuantumParkour.getLevelManager().getLevels().size() + 9) / 10)
+                        .mapToObj(String::valueOf)
                         .collect(Collectors.toList());
             }
         }
