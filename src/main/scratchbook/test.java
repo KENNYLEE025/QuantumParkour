2d4d15f5d0df7596576f9c31c88f61d12bdd6c8c
KENNYLEE025 <153773738+KENNYLEE025@users.noreply.github.com>
2026-03-17 22:01:51 -0500

    Update BlockEventListener fallback categories and sign restore handling

---
+++ src/main/scratchbook/test.java
@@ -0,0 +1,31 @@
+package com.quantumparkour.listener;
+
+
+
+public class test {
+    public static void main(String[] args) {
+        Scaner scanner = new Scanner(System.in);
+        int menuOption = kb.nextInt();
+
+        int base, iv, stat_xp, level;
+        base = kb.nextInt();
+        iv = kb.nextInt();
+        stat_xp = kb.nextInt();
+        level = kb.nextInt();
+        int[] pokemonStats = {base, iv, stat_xp, level};
+        switch (menuOption) {
+            case 1:
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+
+}
+
+public static void calculateLevels()
+{
+
+}
