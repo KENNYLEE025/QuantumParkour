35044a81da0617e750e0983224d1bbe18ab6f74c
KENNYLEE025 <153773738+KENNYLEE025@users.noreply.github.com>
2026-03-18 22:25:25 -0500

    Refactoed PlayerJoinListener.java to improve readability for SQL statements

--- src/main/java/com/quantumparkour/listener/PlayerJoinListener.java
+++ src/main/java/com/quantumparkour/listener/PlayerJoinListener.java
@@ -15,51 +15,84 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class PlayerJoinListener implements Listener {
-
+public class PlayerJoinListener implements Listener
+{
+    //---------------------------------------------------------------------------------------------
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
         Player player = event.getPlayer();
         String uuid = player.getUniqueId().toString();
         String username = player.getName();
+        syncPlayerData(uuid, username);
 
-        // Add or update player in the database
-        try (Connection connection = QuantumDatabase.getConnection()) {
-            // Check if the player already exists in the database
-            String query = "SELECT * FROM Players WHERE UUID = ?";
-            try (PreparedStatement statement = connection.prepareStatement(query)) {
-                statement.setString(1, uuid);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        // Player exists, update the username if it has changed
-                        if (!resultSet.getString("Username").equals(username)) {
-                            String updateQuery = "UPDATE Players SET Username = ? WHERE UUID = ?";
-                            try (PreparedStatement updateStatement = connection.prepareStatement(updateQuery)) {
-                                updateStatement.setString(1, username);
-                                updateStatement.setString(2, uuid);
-                                updateStatement.executeUpdate();
-                            }
-                        }
-                    } else {
-                        // Player does not exist, insert them into the database
-                        String insertQuery = "INSERT INTO Players (UUID, Username, Coins, Rank) VALUES (?, ?, ?, ?)";
-                        try (PreparedStatement insertStatement = connection.prepareStatement(insertQuery)) {
-                            insertStatement.setString(1, uuid);
-                            insertStatement.setString(2, username);
-                            insertStatement.setInt(3, 0); // Default Coins value
-                            insertStatement.setString(4, "Default"); // Default Rank value
-                            insertStatement.executeUpdate();
-                        }
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // Existing functionality
         QuantumParkour.getPlayerManager().add(player);
+        addPlayerToNoCollisionTeam(player);
+    }
 
+    // Helper functions
+    //---------------------------------------------------------------------------------------------
+    private void syncPlayerData(String uuid, String username)
+    {
+        // TODO: FIND A FIX TO ENSURE THAT THE SQL STATEMENTS ARE EASIER TO WRITE AS WE WILL ADD NEW STATISTICS WHICH
+        //       WHICH WILL BE A LOT MORE HARDER MAINTAINING THIS
+        String selectQuery = "SELECT Username FROM Players WHERE UUID = ?";
+        String updateQuery = "UPDATE Players SET Username = ? WHERE UUID = ?";
+        String insertQuery = "INSERT INTO Players (UUID, Username, Coins, Rank) VALUES (?, ?, ? ,?)";
+
+        try (Connection connection = QuantumDatabase.getConnection();
+             PreparedStatement selectStatement = connection.prepareStatement(selectQuery))
+        {
+            selectStatement.setString(1, uuid);
+
+            try (ResultSet resultSet = selectStatement.executeQuery())
+            {
+                if (!resultSet.next())
+                {
+                    insertPlayer(connection, insertQuery, uuid, username);
+                    return;
+                }
+
+                String storedUsername = resultSet.getString("Username");
+
+                if (storedUsername.equals(username))    return;
+
+                updateUsername(connection, updateQuery, uuid, username);
+            }
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    //---------------------------------------------------------------------------------------------
+    private void insertPlayer(Connection connection, String query, String uuid, String username) throws SQLException
+    {
+        try (PreparedStatement statement = connection.prepareStatement(query))
+        {
+            statement.setString(1, uuid);
+            statement.setString(2, username);
+            statement.setInt(3, 0);
+            statement.setString(4, "Default");
+            statement.executeUpdate();
+        }
+    }
+
+    //---------------------------------------------------------------------------------------------
+    private void updateUsername(Connection connection, String query, String uuid, String username)  throws SQLException
+    {
+        try (PreparedStatement statement = connection.prepareStatement(query))
+        {
+            statement.setString(1, uuid);
+            statement.setString(2, username);
+            statement.executeUpdate();
+        }
+    }
+
+    //---------------------------------------------------------------------------------------------
+    private void addPlayerToNoCollisionTeam(Player player)
+    {
         Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
         Team team = scoreboard.getTeam("noCollision");
 
